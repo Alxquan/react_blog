7983c2d36047fa99ff8b190596d55eff24fc10a1
Alex Quan <alxquan@gmail.com>
2017-08-27 11:39:14 -0600

    Moved things around to make them more modular

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,4 +1,12 @@
 var Body = React.createClass({
+  getInitialState(){
+    return{ items: []}
+  },
+
+  componentDidMount() {
+    $.getJSON('api/v1/items.json', (response) => { this.setState({items: response}) });
+  },
+
   handleSubmit(item) {
     var newState= this.state.items.concat(item);
     this.setState({items: newState})
